38aa3505feab99cae31b0365f8f0784dd5d21899
Naijil <naijilaji.14@gmail.com>
2025-08-16 21:45:12 +0530

    Dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,25 @@
+mydict = {'Name':'Pikachu','Type':'Electric','Evolution Level':2,'Trainer':'Ash'}
+print(mydict)
+
+print(mydict.get('Trainer'))
+
+print(mydict.keys())
+print(mydict.values())
+
+for i in mydict.keys():
+    print("Keys",i)
+
+for i in mydict.values():
+    print("Values",i)
+
+cp = mydict.copy()
+print(cp)
+
+cp.clear()
+print(cp)
+
+mydict["Trainer"] = "Ash Ketchum"
+print(mydict)
+
+del mydict["Evolution Level"]
+print(mydict)
